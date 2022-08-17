42ea57174836e6cdddf4abdbd423c0e2c84908fa
dkswami <dkswami1820@gmail.com>
2022-08-17 22:29:28 +0530

    animation added

--- main.js
+++ main.js
@@ -16,5 +16,12 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
-renderer.render(scene, camera);
 
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+animate();
